0b30bacde622eced847e89a385751d5af6c29c19
Caleb Nelson <caleb.nelson@faithlife.com>
2022-04-24 14:30:09 -0700

    Updated open and close so that they are no longer scaled which was causing sub-pixel widths on the stroke which could make the strokes look uneven. Also made stroke width for open and close 2.

--- components/HamburgerMenu.js
+++ components/HamburgerMenu.js
@@ -9,34 +9,40 @@ export const HamburgerMenu = {
 		background: transparent;
 		outline: none;
 		z-index: 1;
-		width: ${({ theme }) => theme.spaces.xxl.replace('rem', '') * 1.2}rem;
-		padding: var(--space-sm);
 		& * {
 			stroke: var(--brand);
-			stroke-width: 1.5;
+			stroke-width: 2;
 		}
 		@media only screen and (max-width: ${({ theme }) => theme.breakpoints.sm}) {
 			display: block;
 
 			svg {
 				height: auto;
-				width: 4rem;
+				width: 4.2rem;
 			}
 		}
 	`,
 	CloseMenuIcon: () => {
 		return (
-			<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 60 70'>
-				<path d='M13 18l33 33m-33 0l33-33' />
-				<path d='M30 0l30 18v33L30 70 0 51V18L30 0' fill='none' />
+			<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 42 48.7'>
+				<path d='M9.7 12.9l22 22m-22 0l22-22' strokeMiterlimit='10' />
+				<path
+					d='M21 .9l20 12v22L21 47.5 1 34.9v-22l20-12'
+					fill='none'
+					strokeMiterlimit='10'
+				/>
 			</svg>
 		);
 	},
 	OpenMenuIcon: () => {
 		return (
-			<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 60 70'>
-				<path d='M10 24h39M10 34.5h39M10 45h39' />
-				<path d='M30 0l30 18v33L30 70 0 51V18L30 0' fill='none' />
+			<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 42 48.7'>
+				<path d='M7.7 16.9h26m-26 7h26m-26 7h26' strokeMiterlimit='10' />
+				<path
+					d='M21 .9l20 12v22L21 47.5 1 34.9v-22l20-12'
+					fill='none'
+					strokeMiterlimit='10'
+				/>
 			</svg>
 		);
 	},
