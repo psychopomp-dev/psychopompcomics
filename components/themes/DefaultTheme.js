4af93121489ec6eae33da414bdf5802b4fc27b87
Caleb Nelson <caleb.nelson@faithlife.com>
2022-07-14 18:48:47 -0700

    Changed the page transitions so they only fade in instead of fading in and moving from the side.

--- components/themes/DefaultTheme.js
+++ components/themes/DefaultTheme.js
@@ -87,7 +87,7 @@ const themeCommon = {
 
 	motion: {
 		pageTransitionVariants: {
-			hidden: { opacity: 0, x: -200, y: 0 },
+			hidden: { opacity: 0, x: 0, y: 0 },
 			visible: { opacity: 1, x: 0, y: 0 },
 			exit: { opacity: 0, x: 0, y: -100 },
 		},
