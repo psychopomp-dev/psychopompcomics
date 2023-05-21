1bb14c98db4a8b1d47677a3101e7b3f8d1c529a7
Caleb Nelson <caleb.nelson@faithlife.com>
2023-05-20 20:55:06 -0700

    WIP Refactored code to store panel index and page index in PsychoReader to facilitate redrawing the canvas on window resize events.

--- components/comicReader/Page.ts
+++ components/comicReader/Page.ts
@@ -2,133 +2,151 @@ import Panel from './Panel';
 import { zoomPan, getDrawImagePropsFromPage } from '../../utils/CanvasHelper';
 
 export interface PageDimension {
-    width: number;
-    height: number;
+	width: number;
+	height: number;
 }
 
 interface Point {
-    x: number;
-    y: number;
+	x: number;
+	y: number;
 }
 
 interface PageOptions {
-    imageUrl: string;
-    panels: Panel[];
-    currentPanel?: number;
-    pageDimensions: PageDimension;
-    center?: Point;
+	imageUrl: string;
+	panels: Panel[];
+	// currentPanel?: number;
+	pageDimensions: PageDimension;
+	center?: Point;
 }
 
 class Page {
-    imageUrl: string;
-    displayImage: string;
-    panels: Panel[];
-    currentPanel: number;
-    pageDimensions: PageDimension;
-    center?: Point;
-    ref?: React.RefObject<HTMLCanvasElement>;
+	imageUrl: string;
+	displayImage: string;
+	panels: Panel[];
+	// currentPanel: number;
+	pageDimensions: PageDimension;
+	center?: Point;
+	ref?: React.RefObject<HTMLCanvasElement>;
 
-    constructor(options: PageOptions) {
-        const { imageUrl, panels, pageDimensions, center } = options;
-        this.imageUrl = imageUrl;
-        this.displayImage = imageUrl;
-        this.panels = panels;
-        this.currentPanel = -1;
-        this.pageDimensions = pageDimensions;
-        this.center = center;
-        if (this.center === undefined) {
-            this.center = {
-                x: this.pageDimensions.width / 2,
-                y: this.pageDimensions.height / 2
-            };
-        }
-    }
+	constructor(options: PageOptions) {
+		const { imageUrl, panels, pageDimensions, center } = options;
+		this.imageUrl = imageUrl;
+		this.displayImage = imageUrl;
+		this.panels = panels;
+		// this.currentPanel = -1;
+		this.pageDimensions = pageDimensions;
+		this.center = center;
+		if (this.center === undefined) {
+			this.center = {
+				x: this.pageDimensions.width / 2,
+				y: this.pageDimensions.height / 2,
+			};
+		}
+	}
 
-    isOnPageImage() {
-        return this.currentPanel === -1;
-    }
+	isOnPageImage(currentPanel: number) {
+		return currentPanel === -1;
+	}
 
-    nextImage() {
-        this.goToNextPanel();
-    }
+	// nextImage(currentPanel: number) {
+	// 	return this.hasNextPanel(currentPanel) ? currentPanel + 1 : currentPanel;
+	// }
 
-    prevImage() {
-        this.goToPrevPanel();
-    }
+	// prevImage(currentPanel: number) {
+	// 	return this.hasPrevPanel(currentPanel) ? currentPanel - 1 : currentPanel;
+	// }
 
-    hasPanels() {
-        return this.panels.length > 0;
-    }
+	hasPanels() {
+		return this.panels.length > 0;
+	}
 
-    goToNextPanel() {
-        if (this.hasNextPanel()) {
-            this.nextPanel();
-            console.log('currentPanel NOW: ' + this.currentPanel);
-        }
-    }
+	goToNextPanel(currentPanel: number) {
+		if (this.hasNextPanel(currentPanel)) {
+			currentPanel = this.nextPanel(currentPanel);
+			console.log('currentPanel NOW: ' + currentPanel);
+			return currentPanel;
+		}
+	}
 
-    goToPrevPanel() {
-        if (this.hasPrevPanel()) {
-            this.prevPanel();
-            console.log('currentPanel NOW: ' + this.currentPanel);
-        }
-    }
+	goToPrevPanel(currentPanel: number) {
+		if (this.hasPrevPanel(currentPanel)) {
+			currentPanel = this.prevPanel(currentPanel);
+			console.log('currentPanel NOW: ' + currentPanel);
+			return currentPanel;
+		}
+	}
 
-    goToPanel(from: number, to: number) {
-        if (this?.ref?.current) {
-            const canvasPropsOld = getDrawImagePropsFromPage(this, this.ref.current, from);
-            const canvasPropsNew = getDrawImagePropsFromPage(this, this.ref.current, to);
-            zoomPan(this.ref.current, this.imageUrl, canvasPropsOld, canvasPropsNew);
-            this.currentPanel = to;
-            console.log('currentPanel NOW: ' + this.currentPanel);
-        }
-    }
+	goToPanel(from: number, to: number) {
+		if (this?.ref?.current) {
+			const canvasPropsOld = getDrawImagePropsFromPage(
+				this,
+				this.ref.current,
+				from
+			);
+			const canvasPropsNew = getDrawImagePropsFromPage(
+				this,
+				this.ref.current,
+				to
+			);
+			zoomPan(this.ref.current, this.imageUrl, canvasPropsOld, canvasPropsNew);
+			// this.currentPanel = to;
+			console.log('currentPanel NOW: ' + to);
+			return to;
+		}
+	}
 
-    nextPanel() {
-        this.goToPanel(this.currentPanel, this.currentPanel + 1);
-    }
+	nextPanel(currentPanel: number) {
+		return this.goToPanel(currentPanel, currentPanel + 1);
+	}
 
-    hasNextPanel() {
-        return this.currentPanel < this.panels.length - 1;
-    }
+	hasNextPanel(currentPanel: number) {
+		return currentPanel < this.panels.length - 1;
+	}
 
-    prevPanel() {
-        this.goToPanel(this.currentPanel, this.currentPanel - 1);
-    }
+	prevPanel(currentPanel: number) {
+		return this.goToPanel(currentPanel, currentPanel - 1);
+	}
 
-    hasPrevPanel() {
-        return this.currentPanel >= 0;
-    }
+	hasPrevPanel(currentPanel: number) {
+		return currentPanel >= 0;
+	}
 
-    setPanel(panelIdx: number) {
-        if (this.panels[panelIdx] !== undefined) this.currentPanel = panelIdx;
-    }
+	// setPanel(panelIdx: number) {
+	//     if (this.panels[panelIdx] !== undefined) this.currentPanel = panelIdx;
+	// }
 
-    getPanel(panelIdx: number): Panel {
-        if (this.panels[panelIdx] === undefined) throw new Error(`Panel index out of range, given ${panelIdx}, must be between 0 and ${this.panels.length}`);
-        return this.panels[panelIdx];
-    }
+	getPanel(panelIdx: number): Panel {
+		if (this.panels[panelIdx] === undefined)
+			throw new Error(
+				`Panel index out of range, given ${panelIdx}, must be between 0 and ${this.panels.length}`
+			);
+		return this.panels[panelIdx];
+	}
 
-    getCurrentPanel(): Panel {
-        return this.getPanel(this.currentPanel);
-    }
+	getCurrentPanel(currentPanel: number): Panel {
+		return this.getPanel(currentPanel);
+	}
 
-    goToFirstPanel() {
-        if (this.hasPanels()) {
-            this.goToPanel(this.currentPanel, 0);
-        }
-    }
+	goToFirstPanel(currentPanel: number) {
+		if (this.hasPanels()) {
+			this.goToPanel(currentPanel, 0);
+			currentPanel = 0;
+		}
+		return currentPanel;
+	}
 
-    goToLastPanel() {
-        if (this.hasPanels()) {
-            this.goToPanel(this.currentPanel, this.panels.length - 1);
-        }
-    }
+	goToLastPanel(currentPanel: number) {
+		if (this.hasPanels()) {
+			currentPanel = this.goToPanel(currentPanel, this.panels.length - 1);
+		}
+		return currentPanel;
+	}
 
-    goToWholePagePanel() {
-        this.goToPanel(this.currentPanel, -1);
-        console.log('currentPanel NOW whole page Panel: ' + this.currentPanel);
-    }
+	goToWholePagePanel(currentPanel: number) {
+		this.goToPanel(currentPanel, -1);
+		console.log('currentPanel NOW whole page Panel: ' + -1);
+		return -1;
+	}
 }
 
 export default Page;
